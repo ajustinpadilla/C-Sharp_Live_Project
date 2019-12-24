43d193f9f64fef5c1536e78d5aea6d6493c8c32f
deanna.cochener <deanna.cochener@gmail.com>
2019-12-23 16:41:16 -0800

    Add project files.

---
+++ TheatreCMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheatreCMS.Startup))]
+namespace TheatreCMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
