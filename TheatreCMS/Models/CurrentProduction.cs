45768122f519bed945a391dff3019236117a3e20
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-12-31 11:58:24 -0800

    Created CurrentProduction class

---
+++ TheatreCMS/Models/CurrentProduction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS.Models
+{
+    public class CurrentProduction
+    {
+        public int ProductionId { get; set; }
+        public string Title { get; set; }
+        public string Playwright { get; set; }
+        public string Description { get; set; }
+        //public image PromoPhoto { get; set; }
+        public DateTime OpeningDay { get; set; }
+        public DateTime ClosingDay { get; set; }
+        public int Season { get; set; }
+        public List<Role> { get; set; }
+    }
+}
