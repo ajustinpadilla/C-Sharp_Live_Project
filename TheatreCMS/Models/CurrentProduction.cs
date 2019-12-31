ad9781433982366d9698455649d06d8b234d24aa
Jacob Kessler <Kessler_Jacob@hotmail.com>
2019-12-31 12:02:56 -0800

    Corrected Schema source

--- TheatreCMS/Models/CurrentProduction.cs
+++ TheatreCMS/Models/CurrentProduction.cs
@@ -10,11 +10,11 @@ namespace TheatreCMS.Models
         public int ProductionId { get; set; }
         public string Title { get; set; }
         public string Playwright { get; set; }
-        public string Description { get; set; }
-        //public image PromoPhoto { get; set; }
         public DateTime OpeningDay { get; set; }
         public DateTime ClosingDay { get; set; }
-        public int Season { get; set; }
-        public List<Role> { get; set; }
+        public byte[] Image { get; set; }
+        public DateTime ShowtimeEve { get; set; }
+        public DateTime ShowtimeMat { get; set; }
+        public string TicketLink { get; set; }
     }
 }
