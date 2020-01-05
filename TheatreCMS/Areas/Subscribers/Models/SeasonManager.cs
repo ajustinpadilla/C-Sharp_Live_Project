11eae54281302d2369afb67cd04989699abbb4b8
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2020-01-04 18:17:35 -0800

    Added and created SeasonManager Class

---
+++ TheatreCMS/Areas/Subscribers/Models/SeasonManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.ComponentModel.DataAnnotations.Schema;
+using TheatreCMS.Models;
+
+namespace TheatreCMS.Areas.Subscribers.Models
+{
+    public class SeasonManager
+    {
+        [Key]
+        public int NumberSeats { get; set; }
+        public bool BookedCurrent { get; set; }
+        public string FallProd { get; set; }
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{00:00}", ApplyFormatInEditMode = true)]
+        public DateTime FallTime { get; set; }
+        public bool BookedFall { get; set; }
+        public string WinterProd { get; set; }
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{00:00}", ApplyFormatInEditMode = true)]
+        public DateTime WinterTime { get; set; }
+        public bool BookedWinter { get; set; }
+        public string SpringProd { get; set; }
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{00:00}", ApplyFormatInEditMode = true)]
+        public DateTime SpringTime { get; set; }
+        public bool BookedSpring { get; set; }
+        public virtual ApplicationUser Subscriber { get; set; }
+
+    }
+}
