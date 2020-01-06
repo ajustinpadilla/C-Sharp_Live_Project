8eb084c686397a261d52477d124292112665a678
Jacob Kessler <Kessler_Jacob@hotmail.com>
2020-01-06 11:06:44 -0800

    Created classes acording to the Company Wiki Schema

---
+++ TheatreCMS/Models/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using TheatreCMS.Enum;
+
+
+namespace TheatreCMS.Models
+{
+    public class Role
+    {
+        [Key]
+        public int RoleID { get; set; }
+        public Production Play { get; set; }
+        public string Character { get; set; }
+        public PositionEnum Type { get; set; }
+        public CastMember Person { get; set; }
+        public string Details { get; set; }
+
+    }
+}
