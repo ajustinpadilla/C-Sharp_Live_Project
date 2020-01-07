86a6a0595bd0ee81288a288e38b4dd3e6aea22f9
ConnorSharpe <52796555+ConnorSharpe@users.noreply.github.com>
2020-01-07 13:08:58 -0800

    Created production views and controller

---
+++ TheatreCMS/Controllers/ProductionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TheatreCMS.Models;
+
+namespace TheatreCMS.Controllers
+{
+    public class ProductionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Productions
+        public ActionResult Index()
+        {
+            return View(db.Productions.ToList());
+        }
+
+        // GET: Productions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Production production = db.Productions.Find(id);
+            if (production == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production);
+        }
+
+        // GET: Productions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Productions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductionId,Title,Playwright,Description,PromoPhoto,OpeningDay,ClosingDay,Season")] Production production)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productions.Add(production);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(production);
+        }
+
+        // GET: Productions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Production production = db.Productions.Find(id);
+            if (production == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production);
+        }
+
+        // POST: Productions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductionId,Title,Playwright,Description,PromoPhoto,OpeningDay,ClosingDay,Season")] Production production)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(production).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(production);
+        }
+
+        // GET: Productions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Production production = db.Productions.Find(id);
+            if (production == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production);
+        }
+
+        // POST: Productions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Production production = db.Productions.Find(id);
+            db.Productions.Remove(production);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
