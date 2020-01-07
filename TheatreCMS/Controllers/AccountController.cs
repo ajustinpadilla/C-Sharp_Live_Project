b795f19b4a50c76e1b773e649379405862d04cfb
Robert Cunningham <robert@bagpyp.net>
2020-01-07 12:21:02 -0800

    debug sign-in portal

--- TheatreCMS/Controllers/AccountController.cs
+++ TheatreCMS/Controllers/AccountController.cs
@@ -75,7 +75,19 @@ namespace TheatreCMS.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+
+            //LoginViewModel model holds only information entered form user (email, password and remember-me-or-not bool),
+            //However, the built in method used below (SignInManager.PasswordSignInAsync), which verifies login attempts as valid or not, 
+            //requires the UserName of the user whose email address is model.Email
+
+            //So, we can find the user whose Email is model.Email via ->
+            var tempUser = UserManager.FindByEmail(model.Email);
+
+            //and extract their username as a string to pass to SignInManager.PasswordSignInAsync
+            string tempUserName = tempUser.UserName;
+
+            
+            var result = await SignInManager.PasswordSignInAsync(tempUserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
