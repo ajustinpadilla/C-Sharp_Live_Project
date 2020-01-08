7f2718a407bccb121342e2a7b0ab5b696bf84b12
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2020-01-07 16:22:50 -0800

    resubmitting corrections for season manager class

--- TheatreCMS/Areas/Subscribers/Models/SeasonManager.cs
+++ TheatreCMS/Areas/Subscribers/Models/SeasonManager.cs
@@ -12,24 +12,25 @@ namespace TheatreCMS.Areas.Subscribers.Models
     public class SeasonManager
     {
         [Key]
+        public int SeasonManagerId { get; set; }
         public int NumberSeats { get; set; }
         public bool BookedCurrent { get; set; }
         public string FallProd { get; set; }
-        [DataType(DataType.Time)]
-        [DisplayFormat(DataFormatString = "{00:00}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime FallTime { get; set; }
         public bool BookedFall { get; set; }
         public string WinterProd { get; set; }
-        [DataType(DataType.Time)]
-        [DisplayFormat(DataFormatString = "{00:00}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime WinterTime { get; set; }
         public bool BookedWinter { get; set; }
         public string SpringProd { get; set; }
-        [DataType(DataType.Time)]
-        [DisplayFormat(DataFormatString = "{00:00}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime SpringTime { get; set; }
         public bool BookedSpring { get; set; }
-        public virtual ApplicationUser Subscriber { get; set; }
+        public virtual Subscriber Subscriber { get; set; }
 
     }
 }
--- TheatreCMS/Models/IdentityModels.cs
+++ TheatreCMS/Models/IdentityModels.cs
@@ -10,18 +10,20 @@ using TheatreCMS.Models;
 namespace TheatreCMS.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class Subscriber : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<Subscriber> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
         }
+
+        public virtual Subscriber Subscribers { get; set; }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<Subscriber>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
