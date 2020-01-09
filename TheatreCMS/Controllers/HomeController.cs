8b2ca5fc591145b9633dd94124aa358c10ca7baa
Robert Cunningham <robert@bagpyp.net>
2020-01-08 16:00:18 -0800

    implement helper for uploading files

--- TheatreCMS/Controllers/HomeController.cs
+++ TheatreCMS/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using TheatreCMS.Helpers;
 
 namespace TheatreCMS.Controllers
 {
@@ -25,6 +27,39 @@ namespace TheatreCMS.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        //File upload GET and POST controls
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+
+                //TODO
+                //upon success, display image instead of 
+                //ViewBag.Message = "File Uploaded Successfully!!";
+
+                ViewBag.Message = "File uploaded Successfully.";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed.";
+                return View();
+            }
         }
     }
 }
---
+++ TheatreCMS/Helpers/ImageUploader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace TheatreCMS.Helpers
+{
+    public static class ImageUploader
+    {
+        public static Image UploadImage(string filename, out byte[] imageBytes)
+        {
+            //Function that takes in the filname of the image and returns the System.Drawing.Image representation of the file. 
+
+            //image is rendered and stored as Image type
+            Image image = Image.FromFile(filename);
+
+            //converts the image into a Byte Array and stores is at as an out-parameter
+            var imgCon = new ImageConverter();
+            imageBytes = (byte[])imgCon.ConvertTo(image, typeof(byte[]));
+
+            //returns image as Image object
+            return image;
+        }
+    }
+}
