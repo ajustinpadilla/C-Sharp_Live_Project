f905940d7185a1451b8fb8c4d89aca870c6600f1
deanna.cochener <deanna.cochener@gmail.com>
2020-01-09 13:58:57 -0800

    fixed error from conflict resolve

--- TheatreCMS/Areas/Subscribers/Models/SeasonManager.cs
+++ TheatreCMS/Areas/Subscribers/Models/SeasonManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.VisualBasic.ApplicationServices;
 using System.ComponentModel.DataAnnotations.Schema;
 using TheatreCMS.Models;
 
