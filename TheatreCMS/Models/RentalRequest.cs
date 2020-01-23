bc69f9fbe78369f900f9b2ecd4a5976815c9e004
deanna.cochener <deanna.cochener@gmail.com>
2020-01-23 13:54:27 -0800

    minor tweaks

--- TheatreCMS/Models/RentalRequest.cs
+++ TheatreCMS/Models/RentalRequest.cs
@@ -14,23 +14,24 @@ namespace TheatreCMS.Models
         public int RentalRequestId { get; set; }
 
         [StringLength(40, ErrorMessage = "Error. This field is limited to 40 characters.")]
+        [Display(Name = "Contact Person")]
         public string ContactPerson { get; set; }
         
         [StringLength(100, ErrorMessage = "Error. This field is limited to 100 characters.")]
         public string Company { get; set; }
         
         [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy hh:mm}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy hh:mm tt}", ApplyFormatInEditMode = true)]
         [Display(Name = "Start Time")]
         public DateTime StartTime { get; set; }
         
         [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy hh:mm}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy hh:mm tt}", ApplyFormatInEditMode = true)]
         [Display(Name = "End Time")]
         public DateTime EndTime { get; set; }
         
         [Display(Name = "Project Info")]
-        [StringLength(200, ErrorMessage = "Error. This field is limited to 200 characters.")]
+        [StringLength(2000, ErrorMessage = "Error. This field is limited to 2000 characters.")]
         public string ProjectInfo { get; set; }
         
         [StringLength(1000, ErrorMessage = "Error. This field is limited to 1000 characters.")]
