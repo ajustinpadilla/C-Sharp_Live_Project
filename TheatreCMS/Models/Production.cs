d0b90504cc9d9535a5f558c9187bc6976b9d983b
Leo <leoramos1292@gmail.com>
2020-01-29 15:31:25 -0800

    Modify Production model

--- TheatreCMS/Models/Production.cs
+++ TheatreCMS/Models/Production.cs
@@ -16,10 +16,32 @@ namespace TheatreCMS.Models
         public string Title { get; set; }
         public string Playwright { get; set; }
         public string Description { get; set; }
-        public byte[] PromoPhoto { get; set; }
+
+        [Display(Name = "Opening Day")]
         public DateTime OpeningDay { get; set; }
+
+        [Display(Name = "Closing Day")]
         public DateTime ClosingDay { get; set; }
+
+        [Display(Name = "Promo Photo")]
+        public byte[] PromoPhoto { get; set; }
+
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "hh:mm tt", ApplyFormatInEditMode = true)]
+        [Display(Name = "Evening Showtime")]
+        public DateTime? ShowtimeEve { get; set; }
+        
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "hh:mm tt", ApplyFormatInEditMode = true)]
+        [Display(Name = "Matinee Showtime")]
+        public DateTime? ShowtimeMat { get; set; }
+
+        [Display (Name = "Ticket Link")]
+        public string TicketLink { get; set; }
         public int Season { get; set; }
-        public virtual List<Part> Parts { get; set; }
+        public bool IsCurrent { get; set; }
+        public byte[] ShowDays { get; set; }
+        public virtual ICollection<Part> Parts { get; set; }
+        public virtual ICollection<CalendarEvent> Events { get; set; }
     }
 }
