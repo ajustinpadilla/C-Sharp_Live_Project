c8261ce8f22994ec99b844e866ac90a75273f262
s42destroyer <53664408+s42destroyer@users.noreply.github.com>
2020-02-05 12:13:22 -0800

    Utilized the Startup.cs file created dummy production data for the database utilized proper method to avoid duplicated data. Populated all required fields.

--- TheatreCMS/Startup.cs
+++ TheatreCMS/Startup.cs
@@ -148,7 +148,7 @@ namespace TheatreCMS
 			{
 				new Production{Title = "Hamilton", Playwright = "Lin-Manuel Miranda", Description = "This is a musical inspired by the biography " +
 				"Alexander Hamilton by historian Ron Chernow. This musical tells the story of American Founding Father Alexander Hamilton through music " +
-				"that draws heavily from hip hop, as well as R&B, pop, soul, and traditional-style show tune.", OpeningDay = new DateTime(2020, 01, 02, 19, 30, 00),
+				"that draws heavily from hip hop, as well as R&B, pop, soul, and traditional-style show tune. ", OpeningDay = new DateTime(2020, 01, 02, 19, 30, 00),
 				ClosingDay = new DateTime(2020, 01, 30, 19, 30, 00), ShowtimeEve = new DateTime(2020, 01, 02, 19, 30, 00) , ShowtimeMat = new DateTime(2020, 01, 02, 22, 30, 00),
 				TicketLink = "ticketsforyou.com", Season = 1, IsCurrent = true},
 
