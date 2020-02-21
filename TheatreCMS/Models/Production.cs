1aa4992650cdc8a6fbfda7579997cab4b594368b
B-Dana <46331892+B-Dana@users.noreply.github.com>
2020-02-21 14:59:55 -0800

    add ProductionPhotos drop-down for Production Edit page, display default Image, add InverseProperty attribute to virtual properties in Production and ProductionPhotos models

--- TheatreCMS/Models/Production.cs
+++ TheatreCMS/Models/Production.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.Drawing;
-
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TheatreCMS.Models
 {
@@ -18,17 +17,13 @@ namespace TheatreCMS.Models
         public string Description { get; set; }
 
         [Display(Name = "Show Runtime (min)")]
-        public int Runtime { get; set; } = 120;
+        public int Runtime { get; set; } 
         
-
         [Display(Name = "Opening Day")]
         public DateTime OpeningDay { get; set; }
 
         [Display(Name = "Closing Day")]
         public DateTime ClosingDay { get; set; }
-
-        [Display(Name = "Promo Photo")]
-        public virtual ProductionPhotos DefaultPhoto { get; set; }
 
         [DataType(DataType.Time)]
         [DisplayFormat(DataFormatString = "{0:hh:mm tt}", ApplyFormatInEditMode = true)]
@@ -48,9 +43,15 @@ namespace TheatreCMS.Models
         public bool IsCurrent { get; set; }
 
         [Display(Name = "World Premiere")]
-        public bool IsWorldPremiere { get; set; } 
+        public bool IsWorldPremiere { get; set; }
+
+
+        [Display(Name = "Promo Photo")]
+        public virtual ProductionPhotos DefaultPhoto { get; set; }
         public virtual ICollection<Part> Parts { get; set; }
         public virtual ICollection<CalendarEvent> Events { get; set; }
+
+        [InverseProperty ("Production")]
         public virtual ICollection<ProductionPhotos> ProductionPhotos { get; set; }
 
        
