62c7f6e77b4721577f0ce5769cb75cc826fa67c0
Pierson Han <piersonhan@gmail.com>
2020-04-28 15:30:05 -0700

    Seed Production Photos

--- TheatreCMS/Startup.cs
+++ TheatreCMS/Startup.cs
@@ -10,6 +10,7 @@ using TheatreCMS.Models;
 using TheatreCMS.Controllers;
 using System;
 using System.Linq;
+using System.Drawing;
 
 [assembly: OwinStartupAttribute(typeof(TheatreCMS.Startup))]
 namespace TheatreCMS
@@ -22,6 +23,7 @@ namespace TheatreCMS
             createRolesandUsers();
             SeedCastMembers();
 			SeedProductions();
+            SeedProductionPhotos();
         }
 
         private ApplicationDbContext context = new ApplicationDbContext();
@@ -148,37 +150,37 @@ namespace TheatreCMS
 		//Seeding database with dummy Productions
 		private void SeedProductions()
 		{
-            
-			var productions = new List<Production>
-			{
-				new Production{Title = "Hamilton", Playwright = "Lin-Manuel Miranda", Description = "This is a musical inspired by the biography " +
-				"Alexander Hamilton by historian Ron Chernow. This musical tells the story of American Founding Father Alexander Hamilton through music " +
-				"that draws heavily from hip hop, as well as R&B, pop, soul, and traditional-style show tune. ", OpeningDay = new DateTime(2020, 01, 02, 19, 30, 00),
-				ClosingDay = new DateTime(2020, 01, 30, 19, 30, 00), ShowtimeEve = new DateTime(2020, 01, 02, 19, 30, 00) , ShowtimeMat = new DateTime(2020, 01, 02, 22, 30, 00),
-				TicketLink = "ticketsforyou.com", Season = 1, IsCurrent = true},
 
-				new Production{Title = "Phantom of the Opera", Playwright = "Andrew Lloyd Webber & Charles Hart", Description = "Based on a French " +
-				"novel of the same name by Gaston Leroux, its central plot revolves around a beautiful soprano, Christine Daae, who becomes the obesession " +
-				"of a mysterious, disfigured musical genius living in the subterranean labyrinth beneath the Paris Opera House.", OpeningDay = new DateTime(2020, 01, 04, 17, 30, 00),
-				ClosingDay = new DateTime(2020, 02, 02, 17, 30, 00), ShowtimeEve = new DateTime(2020, 01, 04, 17, 30, 00), ShowtimeMat = new DateTime(2020, 01, 04, 19, 30, 00),
-				TicketLink = "ticketsforyou.com", Season = 1, IsCurrent = true},
+            var productions = new List<Production>
+            {
+                new Production{Title = "Hamilton", Playwright = "Lin-Manuel Miranda", Description = "This is a musical inspired by the biography " +
+                "Alexander Hamilton by historian Ron Chernow. This musical tells the story of American Founding Father Alexander Hamilton through music " +
+                "that draws heavily from hip hop, as well as R&B, pop, soul, and traditional-style show tune. ", OpeningDay = new DateTime(2020, 01, 02, 19, 30, 00),
+                ClosingDay = new DateTime(2020, 01, 30, 19, 30, 00), ShowtimeEve = new DateTime(2020, 01, 02, 19, 30, 00) , ShowtimeMat = new DateTime(2020, 01, 02, 22, 30, 00),
+                TicketLink = "ticketsforyou.com", Season = 1, IsCurrent = true, DefaultPhoto = context.ProductionPhotos.Where(photo => photo.ProPhotoId == 2).FirstOrDefault()},
 
-				new Production{Title = "The Book of Mormon", Playwright = "Trey Parker, Robert, Lopez, & Matt Stone", Description = "The Book of Mormon " +
-				"follows two Latter-Day Saints missionaries as they attempt to preach the faith of the Church of Jesus Christ of Latter-Day Saints to the " +
-				"inhabitants of a remote Ugandan village.", OpeningDay = new DateTime(2020, 04, 02, 19, 30, 00), ClosingDay = new DateTime(2020, 04, 27, 19, 30, 00),
-				ShowtimeEve = new DateTime(2020, 04, 02, 19, 30, 00), ShowtimeMat = new DateTime(2020, 04, 02, 22, 30, 00), TicketLink = "ticketsforyou.com", Season = 2,
-				IsCurrent = true},
+                new Production{Title = "Phantom of the Opera", Playwright = "Andrew Lloyd Webber & Charles Hart", Description = "Based on a French " +
+                "novel of the same name by Gaston Leroux, its central plot revolves around a beautiful soprano, Christine Daae, who becomes the obesession " +
+                "of a mysterious, disfigured musical genius living in the subterranean labyrinth beneath the Paris Opera House.", OpeningDay = new DateTime(2020, 01, 04, 17, 30, 00),
+                ClosingDay = new DateTime(2020, 02, 02, 17, 30, 00), ShowtimeEve = new DateTime(2020, 01, 04, 17, 30, 00), ShowtimeMat = new DateTime(2020, 01, 04, 19, 30, 00),
+                TicketLink = "ticketsforyou.com", Season = 1, IsCurrent = true, DefaultPhoto = context.ProductionPhotos.Where(photo => photo.ProPhotoId == 3).FirstOrDefault()},
+
+                new Production{Title = "The Book of Mormon", Playwright = "Trey Parker, Robert, Lopez, & Matt Stone", Description = "The Book of Mormon " +
+                "follows two Latter-Day Saints missionaries as they attempt to preach the faith of the Church of Jesus Christ of Latter-Day Saints to the " +
+                "inhabitants of a remote Ugandan village.", OpeningDay = new DateTime(2020, 04, 02, 19, 30, 00), ClosingDay = new DateTime(2020, 04, 27, 19, 30, 00),
+                ShowtimeEve = new DateTime(2020, 04, 02, 19, 30, 00), ShowtimeMat = new DateTime(2020, 04, 02, 22, 30, 00), TicketLink = "ticketsforyou.com", Season = 2,
+                IsCurrent = true, DefaultPhoto = context.ProductionPhotos.Where(photo => photo.ProPhotoId == 6).FirstOrDefault()},
 
 				new Production{Title = "Wicked", Playwright = "Stephen Schwartz", Description = "This musical is told from the perspective of the witches of " +
 				"the Land of Oz; its plot begins before and continues after Dorothy Gale arrives in Oz from Kansas, and includes several references to the 1939 film.",
 				OpeningDay = new DateTime(2019, 10, 01, 19, 30, 00), ClosingDay = new DateTime(2019, 10, 25, 19, 30, 00), ShowtimeEve = new DateTime(2019, 10, 01, 19, 30, 00),
-				ShowtimeMat = new DateTime(2019, 10, 01, 23, 30, 00), TicketLink = "ticketsforyou.com", Season = 4, IsCurrent = false},
+				ShowtimeMat = new DateTime(2019, 10, 01, 23, 30, 00), TicketLink = "ticketsforyou.com", Season = 4, IsCurrent = false, DefaultPhoto = context.ProductionPhotos.Where(photo => photo.ProPhotoId == 7).FirstOrDefault()},
 
 				new Production{Title = "How to Succeed in Business Without Really Trying", Playwright = "Frank Loesser", Description = "This story concerns young, " +
-				"ambitious J. Pierrepont Finch, who, with the help of the book How to Succeed in Business Without Really Trying, rises from window washer to chairman of " +
-				"the board of the World Wide Wicket Company.", OpeningDay = new DateTime(2019, 12, 01, 19, 30, 00), ClosingDay = new DateTime(2019, 12, 22, 19, 30, 00),
-				ShowtimeEve = new DateTime(2019, 12, 01, 19, 30, 00), ShowtimeMat = new DateTime(2019, 12, 01, 23, 30, 00), TicketLink = "ticketsforyou.com", Season = 4,
-				IsCurrent = false},
+                "ambitious J. Pierrepont Finch, who, with the help of the book How to Succeed in Business Without Really Trying, rises from window washer to chairman of " +
+                "the board of the World Wide Wicket Company.", OpeningDay = new DateTime(2019, 12, 01, 19, 30, 00), ClosingDay = new DateTime(2019, 12, 22, 19, 30, 00),
+                ShowtimeEve = new DateTime(2019, 12, 01, 19, 30, 00), ShowtimeMat = new DateTime(2019, 12, 01, 23, 30, 00), TicketLink = "ticketsforyou.com", Season = 4,
+                IsCurrent = false, DefaultPhoto = context.ProductionPhotos.Where(photo => photo.ProPhotoId == 10).First()}
 			};
 
 
@@ -192,10 +194,178 @@ namespace TheatreCMS
                 productions.ForEach(Production => context.Productions.AddOrUpdate(d => d.Title, Production));
                 context.SaveChanges();
             }
-                
-            
-            
-			
 		}
-	}
+
+
+        private void SeedProductionPhotos()
+        {
+            var converter = new ImageConverter();
+            // create images first
+            Image image1 = Image.FromFile(@"~\Content\Images\hamiltonFc.png"); //should it be ~\Content\Images\hamiltonFc.png ??
+            Image image2 = Image.FromFile(@"~\Content\Images\hamilton2.png");
+            Image image3 = Image.FromFile(@"~\Content\Images\phantom1.png");
+            Image image4 = Image.FromFile(@"~\Content\Images\phantom2.png");
+            Image image5 = Image.FromFile(@"~\Content\Images\bookofmormon1.png");
+            Image image6 = Image.FromFile(@"~\Content\Images\bookofmormon2.png");
+            Image image7 = Image.FromFile(@"~\Content\Images\wicked1.png");
+            Image image8 = Image.FromFile(@"~\Content\Images\wicked2.png");
+            Image image9 = Image.FromFile(@"~\Content\Images\howtosucceedinbusinesswithoutreallytrying.png");
+            Image image10 = Image.FromFile(@"~\Content\Images\howtosucceedinbusinesswithoutreallytrying2.png");
+
+            var photos = new List<Photo>
+            {
+                new Photo
+                {
+                    OriginalHeight = image1.Height,
+                    OriginalWidth = image1.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image1, typeof(byte[])),
+                    Title = "Hamilton Image 1"
+                },
+                new Photo
+                {
+                    OriginalHeight = image2.Height,
+                    OriginalWidth = image2.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image2, typeof(byte[])),
+                    Title = "Hamilton Image 2"
+                },
+                new Photo
+                {
+                    OriginalHeight = image3.Height,
+                    OriginalWidth = image3.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image3, typeof(byte[])),
+                    Title = "Phantom Of The Opera Image 1"
+                },
+                new Photo
+                {
+                    OriginalHeight = image4.Height,
+                    OriginalWidth = image4.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image4, typeof(byte[])),
+                    Title = "Phantom Of The Opera Image 2"
+                },
+                new Photo
+                {
+                    OriginalHeight = image5.Height,
+                    OriginalWidth = image5.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image5, typeof(byte[])),
+                    Title = "The Book of Mormon Image 1"
+                },
+                new Photo
+                {
+                    OriginalHeight = image6.Height,
+                    OriginalWidth = image6.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image6, typeof(byte[])),
+                    Title = "The Book of Mormon Image 2"
+                },
+                new Photo
+                {
+                    OriginalHeight = image7.Height,
+                    OriginalWidth = image7.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image7, typeof(byte[])),
+                    Title = "Wicked Image 1"
+                },
+                new Photo
+                {
+                    OriginalHeight = image8.Height,
+                    OriginalWidth = image8.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image8, typeof(byte[])),
+                    Title = "Wicked Image 2"
+                },
+                new Photo
+                {
+                    OriginalHeight = image9.Height,
+                    OriginalWidth = image9.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image9, typeof(byte[])),
+                    Title = "How to Succeed in Business Without Really Trying Image 1"
+                },
+                new Photo
+                {
+                    OriginalHeight = image10.Height,
+                    OriginalWidth = image10.Width,
+                    PhotoFile = (byte[])converter.ConvertTo(image10, typeof(byte[])),
+                    Title = "How to Succeed in Business Without Really Trying Image 2"
+                },
+
+            };
+            photos.ForEach(Photo => context.Photo.AddOrUpdate(p => p.PhotoFile, Photo));
+            context.SaveChanges();
+
+
+
+            var productionphoto = new List<ProductionPhotos>
+            {
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "Hamilton Image 1").FirstOrDefault().PhotoId,
+                    Title = "Hamilton",
+                    Description = "Actors performing \"The Story Of Tonight\".",
+                    Production = context.Productions.Where(name => name.Title == "Hamilton").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "Hamilton Image 2").FirstOrDefault().PhotoId,
+                    Title = "Hamilton",
+                    Description = "Hamilton Cover.",
+                    Production = context.Productions.Where(name => name.Title == "Hamilton").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "Phantom Of The Opera Image 1").FirstOrDefault().PhotoId,
+                    Title = "Phantom Of The Opera",
+                    Description = "Phantom Of The Opera Cover.",
+                    Production = context.Productions.Where(name => name.Title == "Phantom of the Opera").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "Phantom Of The Opera Image 2").FirstOrDefault().PhotoId,
+                    Title = "Phantom Of The Opera",
+                    Description = "The Phantom and Christine embrace.",
+                    Production = context.Productions.Where(name => name.Title == "Phantom of the Opera").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "The Book of Mormon Image 1").FirstOrDefault().PhotoId,
+                    Title = "The Book of Mormon",
+                    Description = "Kevin Price main stage.",
+                    Production = context.Productions.Where(name => name.Title == "The Book of Mormon").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "The Book of Mormon Image 2").FirstOrDefault().PhotoId,
+                    Title = "The Book of Mormon",
+                    Description = "General Butt-F******-Naked and Kevin Price singing together.",
+                    Production = context.Productions.Where(name => name.Title == "The Book of Mormon").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "Wicked Image 1").FirstOrDefault().PhotoId,
+                    Title = "Wicked",
+                    Description = "Wicked Cover",
+                    Production = context.Productions.Where(name => name.Title == "Wicked").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "Wicked Image 2").FirstOrDefault().PhotoId,
+                    Title = "Wicked",
+                    Description = "Elphaba Thropp in the spotlight.",
+                    Production = context.Productions.Where(name => name.Title == "Wicked").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "How to Succeed in Business Without Really Trying Image 1").FirstOrDefault().PhotoId,
+                    Title = "How to Succeed in Business Without Really Trying",
+                    Description = "Smitty and the other secretaries read up on \"How to Hook a Tycoon and Take Him For All He’s Worth\".",
+                    Production = context.Productions.Where(name => name.Title == "How to Succeed in Business Without Really Trying").FirstOrDefault()
+                },
+                new ProductionPhotos
+                {
+                    PhotoId = context.Photo.Where(photo => photo.Title == "How to Succeed in Business Without Really Trying Image 2").FirstOrDefault().PhotoId,
+                    Title = "How to Succeed in Business Without Really Trying",
+                    Description = "Daniel Radcliffe as Pierrepont Finch.",
+                    Production = context.Productions.Where(name => name.Title == "How to Succeed in Business Without Really Trying").FirstOrDefault()
+                }
+            };
+            productionphoto.ForEach(prodphoto => context.ProductionPhotos.AddOrUpdate(p => p.PhotoId, prodphoto));
+            context.SaveChanges();
+        }
+    }
 }
