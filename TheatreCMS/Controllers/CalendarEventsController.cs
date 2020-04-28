49978c8556145418e1498f80fcf7e5666036aed8
SamanthaEpling <alleycatace@yahoo.com>
2020-04-28 09:11:55 -0700

    Commiting changes to update the latest master.

--- TheatreCMS/Controllers/CalendarEventsController.cs
+++ TheatreCMS/Controllers/CalendarEventsController.cs
@@ -23,7 +23,7 @@ namespace TheatreCMS.Controllers
             return View(db.CalendarEvent.ToList());
         }
 
-        public JsonResult FetchEventsAndRenderCalendar()
+        public JsonResult GetCalendarEvents()
         {
             var events = db.CalendarEvent.ToArray();
            
@@ -70,92 +70,104 @@ namespace TheatreCMS.Controllers
       
         }
 
-        //POST:Create/Edit CalendarEvents via Modals
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public JsonResult UpdateEvent(CalendarEvent e)
-        {
-            var status = false;
-            {
-                if (e.EventId >0)
-                {
-                    //Update the event if exists
-                    CalendarEvent calendarEvents = db.CalendarEvent.Where(a => a.EventId == e.EventId).FirstOrDefault();
-                    if (calendarEvents != null)
-                    {
-                        calendarEvents.EventId = e.EventId;
-                        calendarEvents.Title = e.Title;
-                        calendarEvents.StartDate = e.StartDate;
-                        calendarEvents.EndDate = e.EndDate;
-                        calendarEvents.TicketsAvailable = e.TicketsAvailable;
-                        calendarEvents.Color = e.Color;
-                        calendarEvents.ClassName = e.ClassName;
-                        calendarEvents.SomeKey = e.SomeKey;
-                        calendarEvents.AllDay = e.AllDay;
-                        calendarEvents.RentalRequestId = e.RentalRequestId;
-                        calendarEvents.ProductionId = e.ProductionId;
-                    }
+        ////POST:Edit CalendarEvents via Modals
+        //[HttpPost]
+        //[Authorize(Roles = "Admin")]
+        //public JsonResult CreateNewEvent(CalendarEvent e)
+        //{
+        //    var status = false;
+        //    //var status = false;
+        //    //{
+        //    //    if (e.EventId >0)
+        //    //    {
+        //    //        //Update the event if exists
+        //    //        CalendarEvent calendarEvents = db.CalendarEvent.Where(a => a.EventId == e.EventId).FirstOrDefault();
+        //    //        if (calendarEvents != null)
+        //    //        {
+        //    //            calendarEvents.EventId = e.EventId;
+        //    //            calendarEvents.Title = e.Title;
+        //    //            calendarEvents.StartDate = e.StartDate;
+        //    //            calendarEvents.EndDate = e.EndDate;
+        //    //            calendarEvents.TicketsAvailable = e.TicketsAvailable;
+        //    //            calendarEvents.Color = e.Color;
+        //    //            calendarEvents.ClassName = e.ClassName;
+        //    //            calendarEvents.SomeKey = e.SomeKey;
+        //    //            calendarEvents.AllDay = e.AllDay;
+        //    //            calendarEvents.RentalRequestId = e.RentalRequestId;
+        //    //            calendarEvents.ProductionId = e.ProductionId;
+        //    //        }
 
-                }
-                //Create the event if it does not exist yet
-                else
-                {
-                    db.CalendarEvent.Add(e);
-                }
-                db.SaveChanges();
-                status = true;
-            }
-            return new JsonResult { Data = new { status = status } };
-        }
+        //    //    }
+        //    //    //Create the event if it does not exist yet
+        //    //    else
+        //    //    {
+                    
+        //        //}
+        //    db.CalendarEvent.Add(e);
+        //    db.SaveChanges();
+        //    status = true;
+        //    //}
+        //    return new JsonResult { Data = new { status = status } };
+        //}
 
         // POST: CalendarEvents/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for  on, let me show u the 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         [Authorize(Roles = "Admin")]
         public ActionResult Create([Bind(Include = "EventId,Title,StartDate,EndDate,TicketsAvailable,ProductionId,RentalRequestId")] CalendarEvent calendarEvent)
         {
-            ViewData["Productions"] = new SelectList(db.Productions.ToList(), "ProductionId", "Title");
-            ViewData["RentalRequests"] = new SelectList(db.RentalRequests.ToList(), "RentalRequestId", "Company");
 
-            var productionID = Request.Form["Productions"];
-            var rentalID = Request.Form["RentalRequests"];
+            
+            //ViewData["Productions"] = new SelectList(db.Productions.ToList(), "ProductionId", "Title");
+            //ViewData["RentalRequests"] = new SelectList(db.RentalRequests.ToList(), "RentalRequestId", "Company");
 
-            if (productionID == "" && rentalID == "")
+            //var productionID = Request.Form["Productions"];
+            //var rentalID = Request.Form["RentalRequests"];
+
+            //if (productionID == "" && rentalID == "")
+            //{
+            //    var validationMessage = "Please select a Production or a Rental Request.";
+            //    this.ModelState.AddModelError("ProductionId", validationMessage);
+            //    this.ModelState.AddModelError("RentalRequestId", validationMessage);
+            //}
+            //else if (productionID != "" && rentalID == "")
+            //{
+            //calendarEvent.ProductionId = Convert.ToInt32(productionID);
+            //}
+            //else if (productionID == "" && rentalID != "")
+            //{
+            //calendarEvent.RentalRequestId = Convert.ToInt32(rentalID);
+            //}
+            //else
+            //{
+            //    var validationMessage = "You can only select either Production or Rental Request, please try again.";
+            //    this.ModelState.AddModelError("ProductionId", validationMessage);
+            //    this.ModelState.AddModelError("RentalRequestId", validationMessage);
+            //}
+            var isAjax = Request.IsAjaxRequest();
+            if (ModelState.IsValid && !isAjax)
             {
-                var validationMessage = "Please select a Production or a Rental Request.";
-                this.ModelState.AddModelError("ProductionId", validationMessage);
-                this.ModelState.AddModelError("RentalRequestId", validationMessage);
-            }
-            else if (productionID != "" && rentalID == "")
-            {
-                calendarEvent.ProductionId = Convert.ToInt32(productionID);
-            }
-            else if (productionID == "" && rentalID != "")
-            {
-                calendarEvent.RentalRequestId = Convert.ToInt32(rentalID);
-            }
-            else
-            {
-                var validationMessage = "You can only select either Production or Rental Request, please try again.";
-                this.ModelState.AddModelError("ProductionId", validationMessage);
-                this.ModelState.AddModelError("RentalRequestId", validationMessage);
-            }
+                
+                //ViewData["Productions"] = new SelectList(db.Productions.ToList(), "ProductionId", "Title");
+                //ViewData["RentalRequests"] = new SelectList(db.RentalRequests.ToList(), "RentalRequestId", "Company");
 
-            if (ModelState.IsValid)
-            {
-                ViewData["Productions"] = new SelectList(db.Productions.ToList(), "ProductionId", "Title");
-                ViewData["RentalRequests"] = new SelectList(db.RentalRequests.ToList(), "RentalRequestId", "Company");
-
-                if (ViewData["Productions"] != null)
+                //if (ViewData["Productions"] != null)
 
 
-                    db.CalendarEvent.Add(calendarEvent);
+                db.CalendarEvent.Add(calendarEvent);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                System.Diagnostics.Debug.WriteLine("This call was NOT made from ajax, therefore from Create MVC Page. isAjax: " + isAjax);
+                return RedirectToAction("Index");              
             }
-
+            if (ModelState.IsValid && isAjax)
+            {
+                System.Diagnostics.Debug.WriteLine("This call was made from Ajax, therefore from Add Modal. isAjax: " + isAjax);
+                db.CalendarEvent.Add(calendarEvent);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
             return View(calendarEvent);
         }
 
