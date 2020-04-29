a484fb5de902e82eb75ec2afd3da10b62533c514
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-04-28 17:31:57 -0700

    refactor 2

--- TheatreCMS/Controllers/HomeController.cs
+++ TheatreCMS/Controllers/HomeController.cs
@@ -112,96 +112,96 @@ namespace TheatreCMS.Controllers
             return View(productions.ToList());
         }
 
-        private void ArchiveSearch(ApplicationDbContext db, string SearchByCategory, string SearchKey)
+        private void ArchiveSearch(ApplicationDbContext db, string searchByCategory, string searchKey)
         {
-            ViewBag.Category = SearchByCategory;
-            ViewData["SearchKey"] = SearchKey;
-            string highlightedKey = "<span class='bg-primary'>" + SearchKey + "</span>";   //The value of this variable can be altered to change the highlight color of the match.
-            switch (SearchByCategory)
+            ViewBag.Category = searchByCategory;
+            ViewData["SearchKey"] = searchKey;
+            string highlightedKey = "<span class='bg-primary'>" + searchKey + "</span>";   //The value of this variable can be altered to change the highlight color of the match.
+            switch (searchByCategory)
             {
                 case "ArchiveAll":
-                    ViewBag.Message = string.Format("Results for \"{0}\" in Archive", SearchKey);
-                    var resultsCast = db.CastMembers.Where(x => x.Name.ToLower().Contains(SearchKey.ToLower())              //Creates a list of cast members where there are search matches in any of those three columns
-                                                             || x.YearJoined.ToString().Contains(SearchKey.ToLower())
-                                                             || x.Bio.ToLower().Contains(SearchKey.ToLower())).ToList();
+                    ViewBag.Message = string.Format("Results for \"{0}\" in Archive", searchKey);
+                    var resultsCast = db.CastMembers.Where(x => x.Name.ToLower().Contains(searchKey.ToLower())              //Creates a list of cast members where there are search matches in any of those three columns
+                                                             || x.YearJoined.ToString().Contains(searchKey.ToLower())
+                                                             || x.Bio.ToLower().Contains(searchKey.ToLower())).ToList();
                     resultsCast = resultsCast.Distinct().ToList();//Prevents duplicate listings
                     var yearJoinedString = new List<string>();
                     for (int i = 0; i < resultsCast.Count; i++)   //YearJoined must be converted to text to highlight it properly. A separate list is created, then added to the viewbag.
                     { 
-                        resultsCast[i].Name = Regex.Replace(resultsCast[i].Name, SearchKey, highlightedKey, RegexOptions.IgnoreCase);
-                        resultsCast[i].Bio = Regex.Replace(resultsCast[i].Bio, SearchKey, highlightedKey, RegexOptions.IgnoreCase);
+                        resultsCast[i].Name = Regex.Replace(resultsCast[i].Name, searchKey, highlightedKey, RegexOptions.IgnoreCase);
+                        resultsCast[i].Bio = Regex.Replace(resultsCast[i].Bio, searchKey, highlightedKey, RegexOptions.IgnoreCase);
                         yearJoinedString.Add(resultsCast[i].YearJoined.ToString());
-                        yearJoinedString[i] = Regex.Replace(yearJoinedString[i], SearchKey, highlightedKey, RegexOptions.IgnoreCase);
+                        yearJoinedString[i] = Regex.Replace(yearJoinedString[i], searchKey, highlightedKey, RegexOptions.IgnoreCase);
                     }
                     ViewBag.YearJoined = yearJoinedString;
-                    var resultsProduction = db.Productions.Where(x => x.Title.ToLower().Contains(SearchKey.ToLower())
-                                                                   || x.Playwright.ToLower().Contains(SearchKey.ToLower())
-                                                                   || x.Description.ToLower().Contains(SearchKey.ToLower())).ToList();
+                    var resultsProduction = db.Productions.Where(x => x.Title.ToLower().Contains(searchKey.ToLower())
+                                                                   || x.Playwright.ToLower().Contains(searchKey.ToLower())
+                                                                   || x.Description.ToLower().Contains(searchKey.ToLower())).ToList();
                     resultsProduction = resultsProduction.Distinct().ToList();
                     foreach (Production production in resultsProduction)
                     {
-                        production.Title = Regex.Replace(production.Title, SearchKey, highlightedKey);
-                        production.Playwright = Regex.Replace(production.Playwright, SearchKey, highlightedKey);
-                        production.Description = Regex.Replace(production.Description, SearchKey, highlightedKey);
+                        production.Title = Regex.Replace(production.Title, searchKey, highlightedKey);
+                        production.Playwright = Regex.Replace(production.Playwright, searchKey, highlightedKey);
+                        production.Description = Regex.Replace(production.Description, searchKey, highlightedKey);
 
                     }
-                    var resultsPart = db.Parts.Where(x => x.Character.ToLower().Contains(SearchKey.ToLower())
-                                                       || x.Production.Title.ToLower().Contains(SearchKey.ToLower())
-                                                       || x.Person.Name.ToLower().Contains(SearchKey.ToLower())).ToList();
+                    var resultsPart = db.Parts.Where(x => x.Character.ToLower().Contains(searchKey.ToLower())
+                                                       || x.Production.Title.ToLower().Contains(searchKey.ToLower())
+                                                       || x.Person.Name.ToLower().Contains(searchKey.ToLower())).ToList();
                     resultsPart = resultsPart.Distinct().ToList();
                     foreach (Part part in resultsPart)
                     {
-                        part.Character = Regex.Replace(part.Character, SearchKey, highlightedKey);
-                        part.Production.Title = Regex.Replace(part.Production.Title, SearchKey, highlightedKey);
-                        part.Person.Name = Regex.Replace(part.Person.Name, SearchKey, highlightedKey);
+                        part.Character = Regex.Replace(part.Character, searchKey, highlightedKey);
+                        part.Production.Title = Regex.Replace(part.Production.Title, searchKey, highlightedKey);
+                        part.Person.Name = Regex.Replace(part.Person.Name, searchKey, highlightedKey);
                     }
                     if (resultsCast.Count > 0) ViewBag.ResultsCast = resultsCast;                       //sets ViewData value if there were any results
                     if (resultsProduction.Count > 0) ViewBag.ResultsProduction = resultsProduction;
                     if (resultsPart.Count > 0) ViewBag.ResultsPart = resultsPart;
                     break;
                 case "ArchiveCastMember":
-                    ViewBag.Message = string.Format("Results for \"{0}\" in Cast Members", SearchKey);
-                    resultsCast = db.CastMembers.Where(x => x.Name.ToLower().Contains(SearchKey.ToLower())
-                                                         || x.YearJoined.ToString().Contains(SearchKey.ToLower())
-                                                         || x.Bio.ToLower().Contains(SearchKey.ToLower())).ToList();
+                    ViewBag.Message = string.Format("Results for \"{0}\" in Cast Members", searchKey);
+                    resultsCast = db.CastMembers.Where(x => x.Name.ToLower().Contains(searchKey.ToLower())
+                                                         || x.YearJoined.ToString().Contains(searchKey.ToLower())
+                                                         || x.Bio.ToLower().Contains(searchKey.ToLower())).ToList();
                     resultsCast = resultsCast.Distinct().ToList();
                     yearJoinedString = new List<string>();
                     for (int i = 0; i < resultsCast.Count; i++)
                     {
-                        resultsCast[i].Name = Regex.Replace(resultsCast[i].Name, SearchKey, highlightedKey, RegexOptions.IgnoreCase);
-                        resultsCast[i].Bio = Regex.Replace(resultsCast[i].Bio, SearchKey, highlightedKey, RegexOptions.IgnoreCase);
+                        resultsCast[i].Name = Regex.Replace(resultsCast[i].Name, searchKey, highlightedKey, RegexOptions.IgnoreCase);
+                        resultsCast[i].Bio = Regex.Replace(resultsCast[i].Bio, searchKey, highlightedKey, RegexOptions.IgnoreCase);
                         yearJoinedString.Add(resultsCast[i].YearJoined.ToString());
-                        yearJoinedString[i] = Regex.Replace(yearJoinedString[i], SearchKey, highlightedKey, RegexOptions.IgnoreCase);
+                        yearJoinedString[i] = Regex.Replace(yearJoinedString[i], searchKey, highlightedKey, RegexOptions.IgnoreCase);
                     }
                     ViewBag.YearJoined = yearJoinedString;
                     if (resultsCast.Count > 0) ViewBag.ResultsCast = resultsCast;
                     break;
                 case "ArchiveProduction":
-                    ViewBag.Message = string.Format("Results for \"{0}\" in Productions", SearchKey);
-                    resultsProduction = db.Productions.Where(x => x.Title.ToLower().Contains(SearchKey.ToLower())
-                                                               || x.Playwright.ToLower().Contains(SearchKey.ToLower())
-                                                               || x.Description.ToLower().Contains(SearchKey.ToLower())).ToList();
+                    ViewBag.Message = string.Format("Results for \"{0}\" in Productions", searchKey);
+                    resultsProduction = db.Productions.Where(x => x.Title.ToLower().Contains(searchKey.ToLower())
+                                                               || x.Playwright.ToLower().Contains(searchKey.ToLower())
+                                                               || x.Description.ToLower().Contains(searchKey.ToLower())).ToList();
                     resultsProduction = resultsProduction.Distinct().ToList();
                     if (resultsProduction.Count > 0) ViewData["ResultsProduction"] = resultsProduction;
                     foreach (Production production in resultsProduction)
                     {
-                        production.Title = Regex.Replace(production.Title, SearchKey, highlightedKey);
-                        production.Playwright = Regex.Replace(production.Playwright, SearchKey, highlightedKey);
-                        production.Description = Regex.Replace(production.Description, SearchKey, highlightedKey);
+                        production.Title = Regex.Replace(production.Title, searchKey, highlightedKey);
+                        production.Playwright = Regex.Replace(production.Playwright, searchKey, highlightedKey);
+                        production.Description = Regex.Replace(production.Description, searchKey, highlightedKey);
                     }
                     break;
                 case "ArchivePart":
-                    ViewBag.Message = string.Format("Results for \"{0}\" in Parts", SearchKey);
-                    resultsPart = db.Parts.Where(x => x.Character.ToLower().Contains(SearchKey.ToLower())
-                                                   || x.Production.Title.ToLower().Contains(SearchKey.ToLower())
-                                                   || x.Person.Name.ToLower().Contains(SearchKey.ToLower())).ToList();
+                    ViewBag.Message = string.Format("Results for \"{0}\" in Parts", searchKey);
+                    resultsPart = db.Parts.Where(x => x.Character.ToLower().Contains(searchKey.ToLower())
+                                                   || x.Production.Title.ToLower().Contains(searchKey.ToLower())
+                                                   || x.Person.Name.ToLower().Contains(searchKey.ToLower())).ToList();
                     resultsPart = resultsPart.Distinct().ToList();
                     if (resultsPart.Count > 0) ViewData["ResultsPart"] = resultsPart;
                     foreach (Part part in resultsPart)
                     {
-                        part.Character = Regex.Replace(part.Character, SearchKey, highlightedKey);
-                        part.Production.Title = Regex.Replace(part.Production.Title, SearchKey, highlightedKey);
-                        part.Person.Name = Regex.Replace(part.Person.Name, SearchKey, highlightedKey);
+                        part.Character = Regex.Replace(part.Character, searchKey, highlightedKey);
+                        part.Production.Title = Regex.Replace(part.Production.Title, searchKey, highlightedKey);
+                        part.Person.Name = Regex.Replace(part.Person.Name, searchKey, highlightedKey);
                     }
                     break;
                 default:
