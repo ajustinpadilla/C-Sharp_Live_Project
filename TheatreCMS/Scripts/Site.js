ca72963e2db5c066623a470c2bda402b0c92357e
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-05-11 12:26:59 -0700

    remove delay and refactor

--- TheatreCMS/Scripts/Site.js
+++ TheatreCMS/Scripts/Site.js
@@ -42,18 +42,17 @@ if (document.getElementById("main-carousel") != null) {
 //Script for ~/Photo/Index modal
 
 $('.photo-index-image').on('click', function () {
-    setTimeout(function () {
+    
         $('#photo-modal').toggleClass('photo-modal--animation');
         $("body").css('overflow', 'hidden') // removes scrollbar when modal is open
-    }, 100);
+    
 });
 
 $('#photo-modal').on('click', function () {
-    setTimeout(function () {
+    
         $('#photo-modal').toggleClass('photo-modal--animation');
-        $('#photo-modal').toggleClass('photo-modal--close-animation');
         $('body').css('overflow', 'auto')
-    }, 100);
+    
 });
 function ShowModel(id) {
     document.getElementById("photo-modal").style.display = "flex";
