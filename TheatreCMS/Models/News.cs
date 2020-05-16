19428b21f08c74d90f0184aedf23ee05ec086db8
sepan314 <60763020+sepan314@users.noreply.github.com>
2020-05-15 22:02:09 -0400

    add news.cs to models, add controller and crud views

---
+++ TheatreCMS/Models/News.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheatreCMS.Models
+{
+    public class News
+    {
+        [Key]
+        public int NewsId { get; set; }
+        public string Headline { get; set; }
+        public string Content { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? LastSaveDate { get; set; }
+        public DateTime? PublishDate { get; set; }
+        public DateTime? EmailDate { get; set; }
+        public bool Hidden { get; set; }
+    }
+}
