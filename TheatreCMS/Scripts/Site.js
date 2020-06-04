706f63e85d7e4b112f25e1b1e92fb49980ad3b10
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-06-03 17:20:01 -0700

    event list added to modal

--- TheatreCMS/Scripts/Site.js
+++ TheatreCMS/Scripts/Site.js
@@ -49,10 +49,10 @@ function ShowModel(id) {
     var img = document.getElementById("photo-index-img-" + id);
     var modalImg = document.getElementById("photo-modal--content-" + id);
 
-    
+
     modal.style.display = "block";
     modalImg.src = img.src;
-   
+
     var span = document.getElementById("photo-modal--close-" + id);
     span.onclick = function () {
         modal.style.display = "none";
@@ -130,40 +130,44 @@ if (document.getElementById("generate-showtimes-section") != null) {
 
         var modal = $('#bulk-add-modal'),
             btnYes = $('#bulk-add-modal_yes'),
-            btnNo = $('#bulk-add-modal_no');
+            btnNo = $('#bulk-add-modal_no'),
+            pressedYes = false;
 
         modal.show();
+        generateShowtimes();
+
         btnNo.click(function () {
             modal.hide()
         });
         btnYes.click(function () {
-            generateShowtimes();
             modal.hide();
-        })
-         
+            pressedYes = true;
+            generateShowtimes();
+        });
+
 
 
         function generateShowtimes() {
 
-        let production = $("#generate__production-field").children("option").filter(":selected").text(),
-            startDate = moment($("#generate__start-date-field").val()),
-            endDate = moment($("#generate__end-date-field").val()),
-            eventDate = startDate,
-            dateRange = endDate.diff(startDate, 'days'),
-            interval = $("#interval").children("option").filter(":selected").val(),
-            eventList = [],
-            startTimes = [];
-                if ($('#matinee').is(':checked')) {
-                    startTimes.push($('#matinee-time').text());
-                }
-                if ($('#evening').is(':checked')) {
-                    startTimes.push($('#evening-time').text());
-                }
-                if ($('#custom-time').val() != "") {
-                    startTimes.push($('#custom-time').val());
-                }
+            let production = $("#generate__production-field").children("option").filter(":selected").text(),
+                startDate = moment($("#generate__start-date-field").val()),
+                endDate = moment($("#generate__end-date-field").val()),
+                eventDate = startDate,
+                dateRange = endDate.diff(startDate, 'days'),
+                interval = $("#interval").children("option").filter(":selected").val(),
+                eventList = [],
+                startTimes = [];
+            if ($('#matinee').is(':checked')) {
+                startTimes.push($('#matinee-time').text());
+            }
+            if ($('#evening').is(':checked')) {
+                startTimes.push($('#evening-time').text());
+            }
+            if ($('#custom-time').val() != "") {
+                startTimes.push($('#custom-time').val());
+            }
 
-        let productionDays = [];
+            let productionDays = [];
             if ($('#sunday').is(':checked')) {
                 productionDays.push(0);
             }
@@ -186,67 +190,79 @@ if (document.getElementById("generate-showtimes-section") != null) {
                 productionDays.push(6);
             }
 
-        class CalendarEvent {
-            constructor(production, date, dayOfWeek, startTime) {
-                this.production = production;
-                this.date = date;
-                this.dayOfWeek = dayOfWeek;
-                this.startTime = startTime;
-                this.dateString = date;
-            }
-        }
-
-        for (i = 0; i < productionDays.length; i++) {
-            if (productionDays[i] < startDate.day()) {
-                productionDays[i] += 7;
-            }
-            console.log(productionDays[i])
-            startDate.day(productionDays[i]);
-            eventDate = startDate;
-            for (j = productionDays[i]; j <= dateRange + 7; j += 7 * interval) {
-                if (eventDate.isBetween(startDate, endDate, undefined, '[]')) { //check for the eventDate to be within start and end date. The '[]' argument sets it to be inclusive of the start and end date.
-                    for (k = 0; k < startTimes.length; k++) {
-                        console.log(startTimes[k])
-                        console.log(eventDate.format('ll'));
-                        const event = new CalendarEvent(production, moment(eventDate), eventDate.format('dddd'), startTimes[k], eventDate.format('ll'));
-                        eventList.push(event);
-                        
-                    }
+            class CalendarEvent {
+                constructor(production, date, dayOfWeek, startTime) {
+                    this.production = production;
+                    this.date = date;
+                    this.dayOfWeek = dayOfWeek;
+                    this.startTime = startTime;
+                    this.dateString = date;
                 }
-                eventDate.add((7 * interval).toString(), 'days').format('ll');
             }
-            startDate = moment($("#generate__start-date-field").val());
-            eventDate = startDate;
-        }
-        eventList.sort((a, b) => a.date - b.date);
-        console.log(eventList);
 
-        // end block
+            for (i = 0; i < productionDays.length; i++) {
+                if (productionDays[i] < startDate.day()) {
+                    productionDays[i] += 7;
+                }
+                console.log(productionDays[i])
+                startDate.day(productionDays[i]);
+                eventDate = startDate;
+                for (j = productionDays[i]; j <= dateRange + 7; j += 7 * interval) {
+                    if (eventDate.isBetween(startDate, endDate, undefined, '[]')) { //check for the eventDate to be within start and end date. The '[]' argument sets it to be inclusive of the start and end date.
+                        for (k = 0; k < startTimes.length; k++) {
+                            console.log(startTimes[k])
+                            console.log(eventDate.format('ll'));
+                            const event = new CalendarEvent(production, moment(eventDate), eventDate.format('dddd'), startTimes[k], eventDate.format('ll'));
+                            eventList.push(event);
+
+                        }
+                    }
+                    eventDate.add((7 * interval).toString(), 'days').format('ll');
+                }
+                startDate = moment($("#generate__start-date-field").val());
+                eventDate = startDate;
+            }
+            eventList.sort((a, b) => a.date - b.date);
+            console.log(eventList);
+
+            // end block
 
 
-        // This block handles rendering the showtimes table
-        $("#showtimes-container").show();
-        //var columns = 3,
-        //    count = 0,
-        var table = document.getElementById("showtimes-table"),
-            row = table.insertRow();
-        for (i = 0; i < eventList.length; i++) {
-            var cell = row.insertCell();
-            cell.innerHTML = eventList[i].date.format('ll');
-            //count++;
-            cell = row.insertCell();
-            cell.innerHTML = eventList[i].dayOfWeek;
-            //count++;
-            cell = row.insertCell();
-            cell.innerHTML = eventList[i].startTime;
-            //count++;
-            row = table.insertRow();
-            //if (count % columns == 0) {
-            //}
-        }
+            // This block handles rendering the showtimes table
 
-        document.getElementById('showtimes-container').appendChild(table);
-        //$("#showtimes-container").text();
+            if (pressedYes == false) {
+                //$("#showtimes-container").show();
+
+                var table = document.getElementById("modal-table"),
+                    row = table.insertRow();
+                for (i = 0; i < eventList.length; i++) {
+                    var cell = row.insertCell();
+                    cell.innerHTML = eventList[i].date.format('ll');
+                    cell = row.insertCell();
+                    cell.innerHTML = eventList[i].dayOfWeek;
+                    cell = row.insertCell();
+                    cell.innerHTML = eventList[i].startTime;
+                    row = table.insertRow();
+                }
+                document.getElementById('bulk-add-modal_content').appendChild(table);
+            }
+            if (pressedYes == true) {
+                $("#showtimes-container").show();
+                console.log('its true');
+                var table = document.getElementById("showtimes-table"),
+                    row = table.insertRow();
+                for (i = 0; i < eventList.length; i++) {
+                    var cell = row.insertCell();
+                    cell.innerHTML = eventList[i].date.format('ll');
+                    cell = row.insertCell();
+                    cell.innerHTML = eventList[i].dayOfWeek;
+                    cell = row.insertCell();
+                    cell.innerHTML = eventList[i].startTime;
+                    row = table.insertRow();
+                }
+                document.getElementById('showtimes-container').appendChild(table);
+                //$("#showtimes-container").text();
+            }
         }
     });
 }
