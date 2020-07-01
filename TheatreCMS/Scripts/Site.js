99a338967d6c73a7604e4baebfcc5f65401dd74f
gruzzly-bear <facebawx@gmail.com>
2020-07-01 17:05:59 -0400

    remove extra js

--- TheatreCMS/Scripts/Site.js
+++ TheatreCMS/Scripts/Site.js
@@ -436,10 +436,3 @@ if ($("#generate-showtimes-section") != null) {
 }
 // *************************************************************************** End Script for Bulk Add ******************************************************************************************
 // ===========================================================================  CalendarEvents/BulkAdd  =====================================================================================================
-$(function () {
-    $('#showtime-dropdown').change(function () {
-        var readonly = this.value == 'custom-time';
-        $('#matinee').prop('readonly', readonly);
-        $('#evening').prop('readonly', readonly);
-    });
-});
