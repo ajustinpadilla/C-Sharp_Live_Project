d94cc94994fa7f7bcd6db21a2ef4309c1c3fb25b
Patrick R North <60725747+patrickrnorth@users.noreply.github.com>
2020-07-02 23:42:03 -0400

    add First pull request

--- TheatreCMS/Scripts/Site.js
+++ TheatreCMS/Scripts/Site.js
@@ -439,64 +439,57 @@ if ($("#generate-showtimes-section") != null) {
 
 
 
-// ************************************************************************ Rental Request Script *************************************************************************************//
+// ************************************************************************ Rental Request Calendar Script *************************************************************************************//
 
 //calendar Icon toggle 
 function toggleIcon(x) {
-    x.classList.toggle("fa-calendar-check");
+    x.classList.toggle("fa-calendar-plus");
     x.classList.toggle("fa-calendar-minus");
 }
 
 //Toggle calendar show/hide
 $("#toggleCal").click(function () {
-    $("#rentalCalendar").toggle('fast', function () {
+    $("#calendar").toggle('fast', function () {
         $(".log").text('Toggle Transition Complete');
     });
 });
 
-var rentalEvents = [];
-$(document).ready(function () {    
+var rentalEvents = [];  
 
-    //fullCalendar AJAX request for rentalEvent data
-    GetRentalEvents();
-    function GetRentalEvents() {
-        $.ajax({
-            type: "GET",
-            url: "/RentalRequest/GetRentalEvents",
-            success: function (data) {
-                rentalEvents = data
-                GenerateCalendar(rentalEvents);                
-            }
-        })
-    };
+//fullCalendar AJAX request for rentalEvent data
+GetRentalEvents();
+function GetRentalEvents() {
+    $.ajax({
+        type: "GET",
+        url: "/RentalRequest/GetRentalEvents",
+        success: function (data) {
+            rentalEvents = data
+            GenerateCalendar(rentalEvents);                
+        }
+    })
+};
 
-    function GenerateCalendar(rentalEvents) {        
-        //remove the previous calendar
-        $('#rentalCalendar').fullCalendar('destroy');
-        //Assign properties and events to the calendar        
-        $('#rentalCalendar').fullCalendar({
-            contentHeight: 550,
-            timezone: "local",
-            defaultDate: new Date(),          
-            eventLimit: true,
-            events: rentalEvents,                          
-            selectable: true,
-            selectHelper: true,         
-            eventLimit: false,      
-            //pass rentalId parameter to Details method on click
-            eventClick: function getDetails(rentalEvents) {               
-                var requestId = rentalEvents.rentalRequestId;
-                window.location.href = ("/rentalRequest/Details/" + requestId);                
-            }
-        });      
-    };      
-});
-
-
-
-
-
-
-
-
-
+function GenerateCalendar(rentalEvents) {        
+    //remove the previous calendar
+    $('#calendar').fullCalendar('destroy');
+    //Assign properties and events to the calendar        
+    $('#calendar').fullCalendar({        
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek'            
+        },
+        allDaySlot: false,
+        contentHeight: 550,
+        timezone: "local",
+        defaultDate: new Date(),          
+        events: rentalEvents,   
+        eventColor: '#f04d44',
+        eventBorderColor: '#fffbfb',                             
+        //pass rentalId parameter to Details method on click
+        eventClick: function getDetails(rentalEvents) {               
+            var requestId = rentalEvents.rentalRequestId;
+            window.location.href = ("/rentalRequest/Details/" + requestId);                
+        }
+    });      
+};  // ************************************************************************ Rental Request Calendar Script End *************************************************************************************//    
