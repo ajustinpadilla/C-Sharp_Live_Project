aa991ca77a52e254a6b0aa4dbd01d5eec2a9ca3a
GabeSirwinski <gabrielczerwinski@live.com>
2020-07-17 16:02:20 -0700

    fixed <a> tags and took 'edit' out of error message on edit page

--- TheatreCMS/Controllers/PhotoController.cs
+++ TheatreCMS/Controllers/PhotoController.cs
@@ -201,6 +201,8 @@ namespace TheatreCMS.Controllers
         // POST: Photo/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "PhotoId,PhotoFile,OriginalHeight,OriginalWidth,Title")] Photo photo, HttpPostedFileBase file)
